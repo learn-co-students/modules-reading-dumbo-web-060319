41ee2cf1965fa424c9ca4751be683158b6acea58
Alexander Schelchere <ascellie@gmail.com>
2019-06-11 14:50:31 -0400

    two ways to use modules

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+
+    extend MetaDancing # Gives a method to the CLASS
+
+    include Dance # Gives a method the class INSTANCE
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
